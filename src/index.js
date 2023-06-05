738b8c080e32a50b686e6457cf2c1af9d056ddb2
SudipGhara19 <sudipghara135@gmail.com>
2023-06-05 20:54:54 +0530

    added authprovider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import {ToastProvider} from 'react-toast-notifications';
 import './styles/index.css';
 import { App } from './components';
+import { AuthProvider } from './providers/AuthProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <ToastProvider autoDismiss autoDismissTimeout={4000} placement='top-left'>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ToastProvider>
   </React.StrictMode>,
   document.getElementById('root')
