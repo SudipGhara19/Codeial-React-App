1555d97779dd62c3d426066cb333e81b9b1aa391
SudipGhara19 <sudipghara135@gmail.com>
2023-12-09 22:13:58 +0530

    complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,30 @@ import {Link} from "react-router-dom";
 import styles from '../styles/navbar.module.css';
 
 import { useAuth } from "../hooks";
+import { useState, useEffect } from "react";
+import { searchUsers } from "../api";
 
 const Navbar = () => {
+  const [results, setResults] = useState([]);
+  const [searchText, setSearchText] = useState('');
   const auth = useAuth();
+
+  useEffect(() => {
+    const fetchUsers = async() =>  {
+      const response = await searchUsers(searchText);
+
+      if(response.success){
+        setResults(response.data.users)
+      }
+    };
+    if(searchText.length > 2){
+      fetchUsers();
+    }else{
+      setResults([]);
+    }
+  },[searchText])
+
+  
   return (
     <div className={styles.nav}>
       <div className={styles.leftDiv}>
@@ -14,6 +35,23 @@ const Navbar = () => {
             src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
           />
         </a>
+      </div>
+
+      <div className={styles.searchContainer}>
+        <img className={styles.searchIcon} src="https://image.shutterstock.com/image-vector/search-icon-vector-editable-eps10-260nw-1263924991.jpg" alt=""/>
+        <input placeholder="Search User" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+
+        {results.length > 0 && <div className={styles.searchResults}>
+            <ul>
+              {results.map(user => <li className={styles.searchResultsRow} key={`user-${user._id}`}>
+                <Link to={`/user/${user._id}`}>
+                  <img src="https://cdn-icons-png.flaticon.com/128/924/924915.png" alt=""/>
+                  <span>{user.name}</span>
+                </Link>
+              </li>)}
+            </ul>
+          </div>}
+
       </div>
 
       <div className={styles.rightNav}>
